97cd038337da1977315e11871323039b24e35471
Pierre Depaz <pierre.depaz@gmail.com>
2017-06-04 19:56:17 -0400

    successful parsing of board of trustees

--- 01_scraping/d_basic_parser.js
+++ 01_scraping/d_basic_parser.js
@@ -16,20 +16,55 @@ app.get('/content', function(request, response){
 
 
 var request = require('request');
+//in order to parse the data, we need to use the 'cheerio' package
+var cheerio = require('cheerio');
 
 function fetchWebpage(url){
 	console.log('fetching', url, '...');
 	request(url, function(error, response, body){
-		console.log('did we get an error?', error);
+		console.log('requested URL responded with status code:', response.statusCode);
 		console.log('----');
-		console.log('what are the details of our response?', response.statusCode);
-		console.log('----');
-		console.log('since we request a whole html page, might as well display it:');
-		console.log(body);
-		console.log('...DONE!');
+
+		//now that we have the HTML part (in the body variable), we load it into cheerio
+		var $ = cheerio.load(body);
+
+		//first we want to get the headers, so we get ready to store them into an empty array
+		var headers = [];
+		
+		//now we go through all the <h2> elements included inside all the elements with the class="nyurichtexteditor"
+		$('.nyurichtexteditor h2').each(function(index, element){
+			//then we save their text inside the equivalent slot in our array
+			headers[index] = $(this).text();
+		});
+		
+		//and then we print it to our console to see the result
+		console.log(headers);
+
+		//then we do the same things with the names, roles and affiliations
+		var names = [];
+		var roles = [];
+		var affiliations = [];
+
+		$('.nyucolumncontrol .rte p').each(function(index, element){
+			names[index] = $(this).find('b').text();
+			roles[index] = $(this).find('i').text();
+
+			var start_point = $(this).text().indexOf(roles[index]) + roles[index].length + 1;
+			var end_point = $(this).text().length;
+			affiliations[index] = $(this).text().slice(start_point, end_point);
+		});
+
+		console.log(names);
+		console.log(roles);
+		console.log(affiliations);
 	});
 }
 
-var myURL = 'http://yoo.ooo';
+var options = {
+	url: 'https://www.nyu.edu/about/leadership-university-administration/board-of-trustees.html',
+	headers: {
+		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+	}
+};
 
-fetchWebpage(myURL);
+fetchWebpage(options);

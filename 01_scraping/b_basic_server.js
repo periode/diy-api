0d4c535dd308bd21896b82a77bbf4852aac2e5f0
Pierre Depaz <pierre.depaz@gmail.com>
2017-06-06 22:54:22 -0400

    added public folder

--- 01_scraping/b_basic_server.js
+++ 01_scraping/b_basic_server.js
@@ -7,17 +7,18 @@ var port = 2046;
 
 //here we actually run our server, by giving it (1) a port number and (2) a callback function to be executed if the server runs correctly
 app.listen(port, function(){
-	console.log('server started successfully on port', port, 'please visit localhost:',port,'to get content served');
+	console.log('server started successfully on port', port);
+	console.log('please visit localhost:'+port,'to get content served');
 });
 
 //this is where we listen for incoming requests from our users and we respond to those
 //in this particular case, we listen for all requests ('/')
 app.get('/', function(request, response){
 	//and our callback function sends back some text, saying "hey there!"
-	response.send('hey there!');
+	response.send('hey there! this is a server response :)');
 });
 
 app.get('/content', function(request, response){
 	//if the user goes to localhost:2046/content, then we display a regular html file
-	response.sendFile(__dirname + '/index.html');
+	response.sendFile(__dirname + '/public/index.html');
 });

6cd8c29140e4f294d6be6c1fb3d31a520a7059dc
Pierre Depaz <pierre.depaz@gmail.com>
2017-06-15 02:45:54 -0400

    "cleanup"

--- 04_serving_pt2/c_server_templates.js
+++ 04_serving_pt2/c_server_templates.js
@@ -16,22 +16,28 @@ app.set('view engine', 'pug');
 // this is where we specifiy WHERE all of the templates are, usually in a folder we call 'views'
 app.set('views', __dirname+'/public_c/views');
 
-app.get('/', function(req, res, err){
-	res.sendFile('index.html');
+app.get('/', function(request, response, err){
+	response.sendFile('index.html');
 });
 
-app.get('/template', function(req, res, err){
+app.get('/template', function(request, response, err){
 
 	// notice how, here, we are now just giving the name of the template file we want to render
 	// and we use res.render() to render that file
-	res.render('index');
+	response.render('index');
 });
 
-app.get('/custom', function(req, res, err){
+app.get('/custom', function(request, response, err){
 
 	// the powerful thing is that you can then PASS DATA to whatever template you're using
-	// in that case, we're passing it a JSON object, which has one field, 'name', and one value "Pierre"
-	res.render('custom', {name: "Pierre"});
+	// in that case, we're passing it a JSON object, which has two fields, 'name' and 'state', for the respective values "Pierre" and "happy"
+	var data = {
+		name: "Pierre",
+		state: "happy"
+	};
+
+	
+	response.render('custom', data);
 });
 
 

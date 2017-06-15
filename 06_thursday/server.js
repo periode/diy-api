5d7e37c1c613f1a940bd0d231940622136594f05
Pierre Depaz <pierre.depaz@gmail.com>
2017-06-15 13:31:40 -0400

    "html"

--- 06_thursday/server.js
+++ 06_thursday/server.js
@@ -1,1 +1,34 @@
 var Express = require('express');
+
+//this is where we declare our server
+var server = Express();
+
+
+var port = 2046;
+
+server.listen(port, function(){
+  console.log('i am listening on port',port);
+});
+
+server.get('/', function(request, response, error){
+    response.send('hey you are there');
+});
+
+server.get('/pretty', function(request, response, error){
+  response.sendFile('index.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////

58e4240d6326d8cbfcb0371a0280c9870989a2de
Pierre Depaz <pierre.depaz@gmail.com>
2017-06-15 13:45:25 -0400

    "directory"

--- 06_thursday/server.js
+++ 06_thursday/server.js
@@ -1,21 +1,14 @@
 var Express = require('express');
-
+var cheerio = require('cheerio');
 //this is where we declare our server
 var server = Express();
 
-
 var port = 2046;
+
+server.use(Express.static(__dirname + "/public"));
 
 server.listen(port, function(){
   console.log('i am listening on port',port);
-});
-
-server.get('/', function(request, response, error){
-    response.send('hey you are there');
-});
-
-server.get('/pretty', function(request, response, error){
-  response.sendFile('index.html');
 });
 
 
